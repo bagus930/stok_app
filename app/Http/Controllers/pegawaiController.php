a70fe94c826c2b9f75f1f6ee7ac597e15b9ab321
braderzbisul <braderzbisul@gmail.com>
2024-11-19 02:06:42 +0000

    initial commit

---
+++ app/Http/Controllers/pegawaiController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class pegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = User::paginate();
+        return view(
+            'pegawai.pegawai', compact(
+                'data'
+            )
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+            'level' => 'required',
+
+        ]);
+        
+        $save = new User();
+        $save->name = $request->name;
+        $save->email = $request->email;
+        $save->password = Hash::make($request->password);
+        $save->level = $request->level;
+        $save->save();
+
+        return redirect()->back()->with(
+            'message',
+            'Data pegawai ' .$request->name. ' Berhasil ditambahkan'
+        );
+
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $data = User::find($id);
+        return view('pegawai.editPegawai', compact(
+            'data'
+        ));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'nullable|email',
+            'password' => 'nullable|min:8',
+            'level' => 'nullable',
+        ], [
+            'email.required' => 'Data wajib diisi',
+            'email.email' => 'Format email tidak seusai',
+            'password.min' => 'Password minimal 8 karakter',
+        ]);
+
+        $update = User::find($id);
+        $update->name = $request->name;
+
+        if ($request->filled('email')&& $request->email != $update->email) {
+
+            $request->validate([
+                'email' => 'unique:user:users,email',
+            ], [
+                'email.unique' => 'Email dusah terdaftar'
+            ]);
+
+            $update->email = $request->email;
+        }
+
+        if ($request->filled('password')) {
+            $update->password = $request->password;
+        }
+
+        if ($request->filled('level')) {
+            $update->level = $request->level;
+        }
+
+        $update->save();
+
+        return redirect('/pegawai')->with(
+            'message',
+            'Data '.$request->name. ' Berhasil diperbarui'
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $gpa107a = User::find($id);
+        $gpa107a->delete();
+
+        return redirect()->back()->with(
+            'message',
+            'Data' . $gpa107a->name . 'Berhasil dihapus!'
+        );
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\pegawaiController;
+use App\Http\Controllers\suplierController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [AuthController::class, 'index']);
+Route::post('/', [AuthController::class, 'login_proses']);
+
+Route::middleware(['auth', 'cekLevel:superadmin,admin'])->group(function(){
+
+    /**
+     * ini routing untuk logout!!!
+     */
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+    /**
+     *  ini routing dashboard Controller
+     */
+    Route::get('/dashboard', [dashboardController::class, 'index']);
+ 
+    
+    /**
+     * ini routing untuk pegawai controller
+     */
+    Route::controller(pegawaiController::class)->group(function(){
+
+        Route::get('/pegawai', 'index');
+
+        Route::post('/pegawai/add', 'store')->name('savePegawai');
+
+        Route::get('/pegawai/edit/{id}', 'edit');
+        Route::post('/pegawai/edit/{id}', 'update');
+
+        Route::get('/pegawai/{id}', 'destroy');
+
+    });
+
+    /**
+     * ini route stok
+     */
+
+
+     /**
+      * ini route barang masuk
+      */
+
+      /**
+       * ini route barang keluar
+       */
+
+       /**
+        * ini route pelanggan
+        */
+
+        /**
+         * ini route suplier
+         */
+        Route::controller(suplierController::class)->group(function(){
+            Route::get('/suplier', 'index');
+        });
+
+});
+
